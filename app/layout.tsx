db9a3ad7a346e0b7a7ea9aaa4c85f0f1481aadd6
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-08-01 20:04:16 -0300

    creating the userauthform and toast component error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,8 @@ export default function RootLayout({
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
+
+        <Toaster />
       </body>
     </html>
   );
---
+++ components/UserAuthForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function UserAuthForm({
+  className,
+  ...props
+}: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "There was a problem.",
+        description: "There was a error loggin in with Google",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={(cn("flex justify-center"), className)} {...props}>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size={"sm"}
+        className="w-full"
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
+}
